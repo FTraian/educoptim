8a87e1eaf95287111455dbe22c378a2ee4315783
Traian Fratean <traian.fratean@gmail.com>
2015-03-02 22:20:08 +0200

    Cleanup

--- wordpress/backup-3.2.2015_16-24-04_educopti/mysql.sql
+++
@@ -1,5 +0,0 @@
--- cPanel mysql backup
-GRANT USAGE ON *.* TO 'educopti'@'localhost' IDENTIFIED BY PASSWORD '*E8A5FD851B88FCCFEA8F9AEF257CC48AFEBCD37F';
-GRANT ALL PRIVILEGES ON `educopti\_liveDB`.* TO 'educopti'@'localhost';
-GRANT USAGE ON *.* TO 'educopti_liveDB'@'localhost' IDENTIFIED BY PASSWORD '*44D759473EBDA9A7E92CE27DB9B8B847016D3D99';
-GRANT ALL PRIVILEGES ON `educopti\_liveDB`.* TO 'educopti_liveDB'@'localhost';
